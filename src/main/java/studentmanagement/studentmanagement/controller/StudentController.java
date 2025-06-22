c0df1abd846782bbf9cab1caa765607b2a37528c
emera <hippyape7@gmail.com>
2025-06-22 11:54:19 +0900

    ・演習課題14を実施しました。（第23回授業内容、HTMLで検索結果表示）

--- src/main/java/studentmanagement/studentmanagement/controller/StudentController.java
+++ src/main/java/studentmanagement/studentmanagement/controller/StudentController.java
@@ -4,15 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+    //import org.springframework.web.bind.annotation.RestController;
 import studentmanagement.studentmanagement.controller.converter.StudentConverter;
 import studentmanagement.studentmanagement.data.Student;
 import studentmanagement.studentmanagement.data.StudentsCourses;
 import studentmanagement.studentmanagement.domain.StudentDetail;
 import studentmanagement.studentmanagement.service.StudentService;
+import org.springframework.ui.Model;
 
-@RestController
+@Controller
 public class StudentController {
 
   private StudentService service;
@@ -25,11 +27,12 @@ public class StudentController {
   }
 
   @GetMapping("/studentList")
-  public List<StudentDetail> getStudentList() {
+  public String getStudentList(Model model) {
     List<Student> students = service.searchStudentList();
     List<StudentsCourses> studentsCourses = service.searchStudentsCoursesList();
 
-    return converter.convertStudentDetails(students, studentsCourses);
+    model.addAttribute("studentList", converter.convertStudentDetails(students,studentsCourses));
+    return "studentList";
   }
 
   @GetMapping("/studentsCoursesList")
