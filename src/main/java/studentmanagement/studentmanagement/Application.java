91724f9433f008bc6b453397ecf1ffb042b89e67
emera <hippyape7@gmail.com>
2025-06-29 11:54:26 +0900

    課題14のマージ後に残った差分をコミット

--- src/main/java/studentmanagement/studentmanagement/Application.java
+++ src/main/java/studentmanagement/studentmanagement/Application.java
@@ -1,20 +1,21 @@
 package studentmanagement.studentmanagement;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import studentmanagement.studentmanagement.data.Student;
+import studentmanagement.studentmanagement.data.StudentsCourses;
+import studentmanagement.studentmanagement.service.StudentService;
 
 @SpringBootApplication
-@RestController
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 
-	@GetMapping("/hello")
-	public String hello() {
-		return "Hello, World!";
-	}
+
 }
